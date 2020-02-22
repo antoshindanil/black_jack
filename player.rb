646c8679636b96ec2f0fdd1d4e97e50b18e4da48
Danil Antoshin <antoshindanil@ya.ru>
2020-02-22 06:03:43 +0200

    Методы, необходимые для класса игры

--- player.rb
+++ player.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Player
-  attr_reader :name, :cards, :bank
+  attr_reader :name, :cards, :bank, :points
 
-  WIN_POINTS = 21
+  BLACK_JACK = 21
   MAX_CARDS = 3
 
   def initialize(name)
@@ -14,30 +14,41 @@ class Player
   end
 
   def take_card(deck)
-    @cards << deck.take_card
+    card = deck.take_card
+
+    ace_calculate if card.value == 1
+    @points += card.value
+    @cards << card
   end
 
-  def points
-    @cards.each do |card|
-      if card.value == 1
-        ace_calculate
-      end
-      @points += card.value
-    end
-    @points
-  endD
+  def can_take?
+    @cards.length < MAX_CARDS
+  end
 
   def make_bet
     @bank -= 10
   end
 
+  def money?
+    true unless @bank >= 10
+  end
+
+  def increase_money(money)
+    @bank += money
+  end
+
+  def clear_cards
+    @cards.clear
+    @points = 0
+  end
+
   private
 
   def ace_calculate
-    if @points + 11 <= WIN_POINTS
-      @points += 11
-    else
-      @points += 1
-    end
+    @points += if @points + 11 <= BLACK_JACK
+                 11
+               else
+                 1
+               end
   end
 end
